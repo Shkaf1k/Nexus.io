32d27e1de78f5f44e4e9c9993b5c6ee823bde5ae
Shkaf1k <arkadijskurko150@gmail.com>
2026-06-20 17:09:59 +0300

    Update script.js

--- script.js
+++ script.js
@@ -5,9 +5,59 @@ localStorage.getItem("nexusUser")
 
 
 
+let account =
+document.getElementById("account");
+
+
+
 if(user){
 
-document.getElementById("username")
-.innerHTML=user.username;
+
+account.innerHTML = `
+
+
+<div class="profile-menu">
+
+
+<img class="avatar"
+src="${user.avatar}"
+>
+
+
+<div class="user">
+
+
+${user.username}
+
+
+<div class="dropdown">
+
+
+<a href="pages/profile.html">
+Profile
+</a>
+
+
+<a href="#">
+Upload Game
+</a>
+
+
+<a href="#">
+Settings
+</a>
+
+
+</div>
+
+
+</div>
+
+
+</div>
+
+
+`;
+
 
 }
